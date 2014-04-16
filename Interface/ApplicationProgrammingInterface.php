98d83d52de9d3ca8492800d5ce9b089e0586653a
Madobe Ai <AiMadobeAi>
2014-04-16 21:44:54 +0800

    Everything is going smoothly.

--- Interface/ApplicationProgrammingInterface.php
+++ Interface/ApplicationProgrammingInterface.php
@@ -1,20 +1,25 @@
 <?php
-	ini_set('html_errors',false);
-	function Parameter($ParameterKey,$DefaultValue) {
+
+	$Configuration = parse_ini_file('../Configuration/Sira.ini',true);
+	$Connection = mysqli_connect($Configuration['MySQL']['Host'],$Configuration['MySQL']['User'],$Configuration['MySQL']['Pass']);
+
+	function Error($ResponseString='500 Internal Server Error',$ErrorString='Error',$ReturnValue=null) {
+		global $Configuration;
+		header("$_SERVER[SERVER_PROTOCOL] $ResponseString");
+		if($Configuration['Error']['Display']) {
+			header('Content-Type: text/plain');
+			echo $ErrorString; }
+		return $ReturnValue; }
+	function Parameter($ParameterKey,$DefaultValue=null) {
 		if(isset($_GET[$ParameterKey])) { return $_GET[$ParameterKey]; }
 		else if(isset($_POST[$ParameterKey])) { return $_POST[$ParameterKey]; }
 		else { return null; } }
-	$Configuration = parse_ini_file('../Configuration/Sira.ini',true);
-	$Connection = mysqli_connect($Configuration['MySQL']['Host'],$Configuration['MySQL']['User'],$Configuration['MySQL']['Pass']);
-	if(($ConnectionError=mysqli_connect_errno($Connection))) {
-		header("$_SERVER[SERVER_PROTOCOL] 500 Internal Server Error");
-		if($Configuration['MySQL']['VerboseError']) {
-			header('Content-Type: text/plain');
-			echo "MySQL connection error: $ConnectionError"; }
-		exit(1); }
-	// $Document = new DOMDocument('1.0','utf-8');
-	// $APIDocument = $Document->createElement('APIDocument');
-	// $Connection = mysqli_connect('localhost',)
-	// $Document->appendChild($APIDocument);
-	// echo $Document->saveXML();
+
+	if(($ConnectionError=mysqli_connect_errno($Connection))) { exit(Error('500 Internal Server Error',"MySQL Connection Error Number: $ConnectionError",1)); }
+
+	if(Parameter('Action',null)==='Initialise'&&$Configuration['MySQL']['Initialisable']) {
+
+	}
+	else { exit(Error('501 Not Implemented',"Invalid Action",2)); }
+	mysqli_close($Connection);
 ?>
