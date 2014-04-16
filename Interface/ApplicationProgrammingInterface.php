a07a71835f36fe02ec59af667dc02248a57badf7
Madobe Ai <AiMadobeAi>
2014-04-16 19:50:34 +0800

    Removing /System folder and merging all functions into API.

--- Interface/ApplicationProgrammingInterface.php
+++ Interface/ApplicationProgrammingInterface.php
@@ -1,6 +1,13 @@
 <?php
+	ini_set('html_errors',false);
 	function Parameter($ParameterKey,$DefaultValue) {
 		if(isset($_GET[$ParameterKey])) { return $_GET[$ParameterKey]; }
 		else if(isset($_POST[$ParameterKey])) { return $_POST[$ParameterKey]; }
 		else { return null; } }
+	$Configuration = parse_ini_file('../Configuration/Sira.ini');
+	// $Document = new DOMDocument('1.0','utf-8');
+	// $APIDocument = $Document->createElement('APIDocument');
+	// $Connection = mysqli_connect('localhost',)
+	// $Document->appendChild($APIDocument);
+	// echo $Document->saveXML();
 ?>
