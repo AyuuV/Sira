d3643dd5cc0eec40dc55dd0304f13613cf08fcc2
AyuuV <Ayuu.Com.Indus.V@gmail.com>
2014-04-17 21:35:34 +0800

    Just added some tidbits.

--- Interface/ApplicationProgrammingInterface.php
+++ Interface/ApplicationProgrammingInterface.php
@@ -14,8 +14,18 @@ function Parameter($ParameterKey,$DefaultValue=null) {
 	if(isset($_GET[$ParameterKey])) { return $_GET[$ParameterKey]; }
 	else if(isset($_POST[$ParameterKey])) { return $_POST[$ParameterKey]; }
 	else { return null; } }
+function Query($QueryString,$Description='Query',$ErrorCode) {
+	global $Connection;
+	if(!($Result=mysqli_query($Connection,$QueryString))) {
+		Error('500 Internal Server Error',"$Description Failure: ",null);
+		echo mysqli_error($Connection);
+		if(ErrorCode) { exit(ErrorCode); }
+		else { return null; } }
+	else { return $Result; } }
 
-if(($ConnectionError=mysqli_connect_errno($Connection))) { exit(Error('500 Internal Server Error',"MySQL Connection Error Number: $ConnectionError",1)); }
+if(($ConnectionError=mysqli_connect_errno($Connection))) {
+	$ErrorMessage = mysqli_error($Connection);
+	exit(Error('500 Internal Server Error',"MySQL Connection Error $ConnectionError: $ErrorMessage",1)); }
 
 $CurrentDate = mysqli_real_escape_string($Connection,date('Y-m-d H:i:s'));
 $Database = mysqli_real_escape_string($Connection,$Configuration['MySQL']['Database']);
@@ -31,25 +41,24 @@ if(Parameter('Initialise',null)&&$Configuration['MySQL']['Initialisable']) {
 	if(!($TagTableStructure=file_get_contents('../Configuration/Tag Table Structure.txt',false,null,35))) { exit(Error('500 Internal Server Error','Tag Table Structure Access Failure',5)); }
 	if(!($UserTableStructure=file_get_contents('../Configuration/User Table Structure.txt',false,null,36))) { exit(Error('500 Internal Server Error','User Table Structure Access Failure',6)); }
 
-	function Initialise($QueryString,$Description,$ErrorCode) {
-		global $Connection;
-		if(!mysqli_real_query($Connection,$QueryString)) {
-			Error('500 Internal Server Error',"$Description Failure: ",null);
-			echo mysqli_error($Connection);
-			exit($ErrorCode); } }
+	Query("DROP DATABASE IF EXISTS `$Database`;","Database $Database Drop",7);
+	Query("CREATE DATABASE `$Database`;","Database $Database Creation",8);
+	Query("USE `$Database`","Database $Database Usage",9);
+	Query("CREATE TABLE $TagTableStructure;","Tag Table Structure Creation ($TagTableStructure)",12);
+	Query("CREATE TABLE $UserTableStructure;","User Table Structure Creation ($UserTableStructure)",13);
+	Query("CREATE TABLE $ImageTableStructure;","Image Table Structure Creation ($ImageTableStructure)",10);
+	Query("CREATE TABLE $LinkTableStructure;","Link Table Structure Creation ($LinkTableStructure)",11);
+	Query("INSERT INTO `U` (I,N,P,EMA,AC,CDT) VALUES (0,'$Username','$Password','$EMail',18446744073709551615,'$CurrentDate');","Administrator $Username Creation",14); }
 
-	Initialise("DROP DATABASE IF EXISTS `$Database`;","Database $Database Drop",7);
-	Initialise("CREATE DATABASE `$Database`;","Database $Database Creation",8);
-	Initialise("USE `$Database`","Database $Database Usage",9);
-	Initialise("CREATE TABLE $TagTableStructure;","Tag Table Structure Creation ($TagTableStructure)",12);
-	Initialise("CREATE TABLE $UserTableStructure;","User Table Structure Creation ($UserTableStructure)",13);
-	Initialise("CREATE TABLE $ImageTableStructure;","Image Table Structure Creation ($ImageTableStructure)",10);
-	Initialise("CREATE TABLE $LinkTableStructure;","Link Table Structure Creation ($LinkTableStructure)",11);
-	Initialise("INSERT INTO `U` (I,N,P,EMA,AC,CDT) VALUES (0,'$Username','$Password','$EMail',18446744073709551615,'$CurrentDate');","Administrator $Username Creation",14); }
+if(!Parameter('UserIdentifier',null)) { exit(Error('401 Unauthorized','User Identifier Invalid',15)); }
+else {
+	$Identifier = mysqli_real_escape_string($Connection,Parameter('UserIdentifier',null));
 
-if(Parameter('Query',null)==='Download') { }
+}
 
-else { exit(Error('501 Not Implemented',"Invalid Query Action",2)); }
+if(Parameter('Request',null)==='Download') { }
+
+else { exit(Error('501 Not Implemented',"Invalid Request",2)); }
 
 mysqli_close($Connection);
 
