80d4e4c0ab1be4522770d3997099a279cc35735b
AyuuV <Ayuu.Com.Indus.V@gmail.com>
2014-04-29 19:49:53 +0800

    Moved stuff.

--- Interface/Resources/Scripts/Appearance.js
+++ Interface/Resources/Scripts/Appearance.js
@@ -1,3 +1,12 @@
+var appearanceHashChange = function() {
+	var currentLocation = location.hash.split('/');
+	var elementsList = document.getElementById('interfacesParent');
+	var navLinksList = document.getElementById('navLinksList');
+	if(currentLocation) {
+		appearanceHighlightActiveLinks(navLinksList.getElementsByTagName('a'),currentLocation[0],'activeLink');
+		appearanceHighlightActiveElements(elementsList.children,'locationIdentifier',currentLocation[0],'activeInterface'); }
+	return; }
+
 var appearanceHighlightActiveElements = function(elementList,dataAttribute,activeState,activeClass) {
 	for(var index=0;index<elementList.length;index++) {
 		if(elementList[index].dataset[dataAttribute]==activeState) { elementList[index].classList.add(activeClass); }
@@ -9,3 +18,5 @@ var appearanceHighlightActiveLinks = function(linkList,activeLocation,activeClas
 		if(linkList[index].getAttribute('href')==activeLocation) { linkList[index].classList.add(activeClass); }
 		else { linkList[index].classList.remove(activeClass); } }
 	return linkList; }
+
+window.addEventListener('hashchange',appearanceHashChange);
--- Interface/Resources/Scripts/Interface.js
+++ Interface/Resources/Scripts/Interface.js
@@ -1,10 +1,4 @@
 var interfaceHashChange = function() {
-	var currentLocation = location.hash.split('/');
-	var elementsList = document.getElementById('interfacesParent');
-	var navLinksList = document.getElementById('navLinksList');
-	if(currentLocation) {
-		appearanceHighlightActiveLinks(navLinksList.getElementsByTagName('a'),currentLocation[0],'activeLink');
-		appearanceHighlightActiveElements(elementsList.children,'locationIdentifier',currentLocation[0],'activeInterface'); }
 	return; }
 
 var interfaceInitialise = function() {
